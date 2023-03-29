2962d54d89e9d932598e930c2c74775e11ae936d
Vinit Rathod <vinitrathod123@gmail.com>
2023-03-29 05:32:36 +0530

    first commit

---
+++ Hashing/HashLN.java
@@ -0,0 +1,110 @@
+public class HashLN{
+    int bucket,val;
+    HashLN[] table;
+    HashLN next,prev;
+
+    public HashLN(int val, HashLN next, HashLN prev){
+        this.val = val;
+        this.next = next;
+        this.prev = prev;
+    }
+
+    public HashLN(int bucket){
+        this.bucket = bucket;
+        this.table = new HashLN[bucket];
+        for(int i=0;i<bucket;i++){
+            table[i] = new HashLN(-1,null,null);
+        }
+    }
+
+    public int hashFunction(int key){
+        return key % bucket;
+    }
+
+    public void insertItem(int key){
+        int index = hashFunction(key);
+
+        if(table[index].next != null){
+            HashLN head = table[index].next;
+            while(head.next != null){
+                head = head.next;
+            }
+            HashLN temp = new HashLN(key,null,null);
+            temp.prev = head;
+            head.next = temp;
+        }else{
+            HashLN temp = new HashLN(key,null,null);
+            table[index].next = temp;
+            temp.prev = table[index];
+        }
+    }
+
+    public void deleteItem(int key){
+        int index = hashFunction(key);
+
+        if(table[index].next != null){
+            HashLN temp = table[index].next;
+            while(temp != null){
+                if(temp.val == key){
+                    break;
+                }else{
+                    temp = temp.next;
+                }
+            }
+            if(temp != null){
+                if(temp.next == null){
+                    HashLN prev = temp.prev;
+                    prev.next = temp.next;
+                    temp.prev = null;
+                }else if(table[index].next.val == key && table[index].next.next == null){
+                    table[index].next = null;
+                }else{
+                    HashLN prev = temp.prev;
+                    prev.next = temp.next;
+                    temp.prev = null;
+                    temp.next.prev = prev;
+                    temp.next = null;
+                }
+            }else{
+                return;
+            }
+        }
+    }
+
+    public void displayHash(){
+        for(int i=0;i<bucket;i++){
+            System.out.print(i);
+            HashLN temp = table[i].next;
+            while(temp != null){
+                System.out.print(" --> "+temp.val);
+                temp = temp.next;
+            }
+            System.out.println();
+        }
+    }
+
+
+
+    public static void main(String args[]){
+        // array that contains keys to be mapped
+        int[] a = { 15, 11, 27, 8, 12,10,13,18,22,50 };
+ 
+        // Create a empty has of BUCKET_SIZE
+        HashLN h = new HashLN(7);
+ 
+        // insert the keys into the hash table
+        for (int x : a) {
+            h.insertItem(x);
+        }
+
+        // Display the hash table
+        h.displayHash();
+ 
+        // delete 12 from the hash table
+        h.deleteItem(50);
+ 
+        // Display the hash table
+        h.displayHash();
+        // System.out.println(h.table[1].next.prev + " "+h.table[1].next.val);
+    }
+}
---
+++ Stack Using Singly Linked List/StackLL.java
@@ -0,0 +1,64 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Other/File.java to edit this template
+ */
+
+/**
+ *
+ * @author Vinit
+ */
+public class StackLL {
+
+    ListNode head = new ListNode();
+    public void Push(int pushVal){
+        if(head == null){
+            head = new ListNode(pushVal);
+        }else{
+            ListNode temp = new ListNode(pushVal);
+            temp.next = head;
+            head = temp;
+        }
+    }
+    
+    
+    public int Pop(){
+        ListNode temp = head;
+        if(temp == null){
+            return -1;
+        }else{
+            head = head.next;
+            temp.next = null;
+        }
+        return temp.val;
+    }
+    public void traverseLL(ListNode head){
+        ListNode temp = head;
+        while(temp != null){
+            System.out.println(temp.val);
+            temp = temp.next; 
+        }
+    }
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String args[]) {
+        // TODO code application logic here
+        StackLL obj = new StackLL();
+        obj.Push(1);
+        obj.traverseLL(head);
+    }
+}
+
+public class ListNode{
+    int val;
+    ListNode next;
+    
+    ListNode(){}
+    ListNode(int val){
+        this.val = val;
+    }
+    ListNode(int val, ListNode next){
+        this.val = val;
+        this.next = next;
+    }
+}
